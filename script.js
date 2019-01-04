2aa96f1ccf23c3cf9870bc3b9efb7c984503087e
Ben Zobrist <ben@zobie.com>
2019-01-04 13:17:16 -0700

    Updated slideshow
    Sizes self properly, link colors change, images size properly.

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
+function plusSlides(n)
+{
   showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
+function currentSlide(n)
+{
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides(n)
+{
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
